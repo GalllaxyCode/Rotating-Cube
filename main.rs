10ab867e1b0782cdba1b1e761491b098ca8cd9a3
Galllaxy <topaccfort@mail.ru>
2025-01-22 21:46:08 +0300

    first commit

---
+++ main.rs
@@ -0,0 +1,14 @@
+const SCREEN_H: usize = 80;
+const SCREEN_W: usize = 160;
+const HALF_KSIZE: f64 = 20.0;
+const K1: usize = 40;
+const INCREMENT_SPEED: f64 = 0.6
+const ROTATE_X_SPEED: f64 = 0.04
+const ROTATE_Y_SPEED: f64 = 0.04
+
+fn main() {
+    print!("\x1b[2J");
+    let mut a: f64 = 0.0
+    let mut b: f64 = 0.0
+    let mut c: f64 = 0.0
+}
